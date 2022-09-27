5f57bca774ceaaf89a5847df59f144df70c8a230
Matheus Silva <mathengs@hotmail.com>
2022-09-26 22:29:33 -0300

    Adicionado validação para email

--- src/main/java/com/facens/troca/online/api/dto/user/UserRegisterDTO.java
+++ src/main/java/com/facens/troca/online/api/dto/user/UserRegisterDTO.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -12,12 +13,14 @@ import javax.validation.constraints.Size;
 public class UserRegisterDTO {
 
     @NotNull(message = "Username must be not null")
+    @Size(max = 20, message = "Username length must be less than 20")
     private String username;
-    private Long roleId;
     @NotNull(message = "Email must be not null")
+    @Size(max = 30, message = "Email length must be less than 30")
+    @Email(message = "Email must be valid")
     private String email;
     @NotNull(message = "Password must be not null")
-    @Size(min = 6, max = 50, message = "Password Length must be between 6 and 50")
+    @Size(min = 6, max = 50, message = "Password length must be between 6 and 50")
     private String password;
     private String photoUrl;
 
